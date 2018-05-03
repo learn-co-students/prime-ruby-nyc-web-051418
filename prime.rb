ca0c346151b44947e0a936cb2019dc7c8342de72
Jordan Eckowitz <jordan.eckowitz@gmail.com>
2018-05-03 20:07:17 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,27 @@
 # Add  code here!
+def prime?(number)
+
+  if number <= 1
+    return false
+  elsif number == 2
+    return true
+  elsif number == 3
+    return true
+  else 
+  
+  prime = true
+  
+  x = 2 
+  
+  while x <= number/2
+    if number%x == 0
+      prime = false
+      break
+    end
+    x += 1
+  end
+  
+  return prime
+  
+  end
+end 
