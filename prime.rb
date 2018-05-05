09ae5d9cb070cf4ae39f908f5c35775ffa430135
Kayla Fitzgerald <fitzgerald.km11@gmail.com>
2018-05-05 16:09:11 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,13 @@
-# Add  code here!
+def prime?(integer)
+  if integer < 2
+    return false
+  end
+
+  possible_factors = (2...integer).to_a
+  primes_found = []
+  possible_factors.each do |number|
+    primes_found << integer % number
+  end
+
+  primes_found.include?(0) ? false : true
+end
